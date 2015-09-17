f011ba5bc50d9dcdcd4dcebc0a9098ec6771bb7d
cts2003258 <cts2003258@gmail.com>
2015-09-17 16:10:40 -0500

    iloveyouhan

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.order(:title, :release_date)
+    @movies = Movie.all(:order => "title ASC, release_date ASC")
   end
 
   def new
