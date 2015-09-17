30efc22832438cc281fd3f62c9fe93af55a1e1b7
cts2003258 <cts2003258@gmail.com>
2015-09-17 16:38:00 -0500

    iloveyouhan

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all.sort_by { |movie| movie.title }
+    @movies = Movie.order(":title DESC")
   end
 
   def new
