79770331387e99052c39359b5b48cb86821d6370
cts2003258 <cts2003258@gmail.com>
2015-09-18 00:40:10 -0500

    hope

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
 	def sortable(column, title = nil)
 		title ||= column.titleize
 		#css_class = column == sort_column ? "hilite" : nil
-		link_to title, {:sort => column}, {:class => th.hilite}
+		link_to title, {:sort => column}, {:class => "hilite"}
 	end
 end
