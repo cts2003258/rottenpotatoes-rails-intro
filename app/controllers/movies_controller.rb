f2519ecda97d9558fa14743cb2dbe96d12bca173
cts2003258 <cts2003258@gmail.com>
2015-09-17 23:58:10 -0500

    hope

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -46,6 +46,6 @@ class MoviesController < ApplicationController
   private
 
   def sort_column
-    params[:sort] || "title"
+    ["title" "release_date"].include?(params[:sort]) ? params[:sort] : "title"
   end
 end
